f87c3ce8f9016410ec4157153fff9cfb57704b14
Davi-Bandeira <davibandeira2011@hotmail.com>
2024-04-19 11:22:28 -0300

    Start Invest Project

---
+++ src/main/java/tech/investmentProject/InvestmentProjectApplication.java
@@ -0,0 +1,13 @@
+package tech.investmentProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentProjectApplication.class, args);
+	}
+
+}
